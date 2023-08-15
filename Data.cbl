da6ef6eb6934e692198c53cba05835a5db586848
MattyTheHacker <18513864+MattyTheHacker@users.noreply.github.com>
2023-08-15 15:26:11 +0100

    Something new

---
+++ Data.cbl
@@ -0,0 +1,7 @@
+       working-storage section.
+       01 string-variable pic X(20) value "Hello World".
+
+       PROCEDURE DIVISION.
+           display string-variable
+           stop run
+           .
---
+++ Hello World.cbl
@@ -0,0 +1,9 @@
+      $set dialect(ANS85)
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO-WORLD.
+
+       PROCEDURE DIVISION.
+       100-PROGRAM.
+           DISPLAY 'Hello World!'
+           STOP RUN
+           .
